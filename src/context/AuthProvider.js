77eb59bc06a980b7a28452f7073d8a0d3678e8c7
Aritra Basu <43692750+aritrakrbasu@users.noreply.github.com>
2021-08-29 19:21:15 +0530

    admin portal

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -14,7 +14,6 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true)
 
   function createUser(email, password, name,phone_number,file) {
-    console.log(file)
     return new Promise((resolve, reject) => {
       auth.createUserWithEmailAndPassword(email, password).then(function(result) {
         const user = auth.currentUser;
@@ -72,100 +71,6 @@ export function AuthProvider({ children }) {
     })
   }
 
-  function hospitalLogin(email, password) {
-    return new Promise((resolve, reject) => {
-      db.collection("users").where("email","==",email).get().then((docs)=>{
-        if(docs.empty)
-        {
-          reject({message:"no such account exists"})
-        }
-        else
-        {
-          docs.forEach(doc=>{
-            if(doc.exists)
-            {
-                if(doc.data().isHospital)
-                {
-                  auth.signInWithEmailAndPassword(email, password).then(() => {
-                    resolve()
-                  })
-                  .catch(error => reject(error));
-                }
-                else
-                {
-                  reject({message:"Not a hospital"})
-                }
-            }
-            else
-            {
-              reject({message:"no such account exists"})
-            }
-          })
-        }
-      })
-    })
-  }
-
-  function registerPolice(email, password, Pname) {
-    return new Promise((resolve, reject) => {
-      auth.createUserWithEmailAndPassword(email, password).then(function(result) {
-        const user = auth.currentUser;
-        user.updateProfile({
-          displayName: Pname
-        }).then((result)=>{
-          db.collection("users").doc(user.uid).set({
-            displayName:Pname,
-            uid:user.uid,
-            email:email,
-            isPolice:true,
-          }).then(()=>{
-
-            db.collection("policeList").doc("policeList").set({
-              newRequest:{},
-              acceptedRequests:{},
-              rejectedRequests:{},
-              activeRecords:{},
-              acceptedRequestCount:0,
-              RejectedRequestCount:0,
-              newRequestCount:0
-            }).then(()=> resolve())
-            .catch(error => reject("error creating profile"));
-          })
-          .catch(error => reject("error creating profile"));
-        })
-        .catch(error => reject(error));
-      })
-      .catch(error => reject(error));
-    })
-  }
-
-  function policeLogin(email, password) {
-    return new Promise((resolve, reject) => {
-      db.collection("users").where("email","==",email).get().then((docs)=>{
-        docs.forEach(doc=>{
-          if(doc.exists)
-        {
-            if(doc.data().isPolice)
-            {
-              auth.signInWithEmailAndPassword(email, password).then(() => {
-                resolve()
-              })
-              .catch(error => reject(error));
-            }
-            else
-            {
-              reject({message:"Not Police Station"})
-            }
-        }
-        else
-        {
-          reject({message:"No such Police Stations Exist"})
-        }
-        })
-      })
-    })
-  }
-
   function logout() {
     sessionStorage.clear();  
         setCurrentUser()
@@ -189,13 +94,18 @@ export function AuthProvider({ children }) {
       if(user && user.uid)
       {
         db.collection("users").doc(user.uid).onSnapshot((docs)=>{
+          console.log(docs)
           if(docs.exists)
             {
               setCurrentUser(docs.data())
               setLoading(false)
+            }else
+            {
+              logout()
             }
           })
       }else
+        logout()
         setLoading(false)
       
     })
--- src/dashboard.js
+++ src/dashboard.js
@@ -7,6 +7,7 @@ import { db } from './firebase'
 import EventRegistrationModal from './components/EventRegistrationModal'
 import { useAuth } from './context/AuthProvider'
 import SubmissionForm from './components/SubmissionForm'
+import AdminDashboard from './components/AdminDashboard'
 
 
 
@@ -81,104 +82,121 @@ function Dashboard() {
         }
     },[currentUser])
     
+    {console.log(currentUser)}
+    
     return (
-        <Container fluid={true}>
+        <>
+        {
+            
+                <>
+                <Container fluid={true}>
           <Row>
             <Col lg={1}>
             <Sidebar />
             </Col>
             <Col lg={11}>
-            <Container fluid>
-            <Row className="mt-5">
-                {
-                    (registeredEvents.includes("thewallarticle") || registeredEvents.includes("thewallartwork") 
-                    || registeredEvents.includes("thewallpoetry") || registeredEvents.includes("shutterbugshortvideos") 
-                    || registeredEvents.includes("shutterbugphotos")) && (
-                    <h1 className="section-heading">Submission</h1>
-                    )
-                }
-                
-            {events && events.length>0 && events.map((event,index)=>{
-                if(index > 0)
-                {
-                    
-                }
-                if( registeredEvents.includes(event.id))
-                {
-                    if(event.id === "thewallarticle"|| event.id === "thewallartwork"||event.id === "thewallpoetry"||event.id ==="shutterbugshortvideos"||event.id ==="shutterbugphotos")
-                    {
-                        return(
-                            <Col lg={2} xs={6} md={2} className="item-container" key={index}>
-                            <div className="item-holder ">
-                            <Button className="event_register p-0 " onClick={()=>showForm(event)}> 
-                                <img src={`./icon/${event.id}.png`} className="item-holder-image img-fluid" />
-                                </Button>
-                                <div className="item__nameStyle">{event.name}</div>
-                            </div>
+                {currentUser.isCoordinator ? (
+                <AdminDashboard />
+                ):(
+                    <>
+                    <Container fluid>
+                    <Row className="mt-5">
+                        {
+                            (registeredEvents.includes("thewallarticle") || registeredEvents.includes("thewallartwork") 
+                            || registeredEvents.includes("thewallpoetry") || registeredEvents.includes("shutterbugshortvideos") 
+                            || registeredEvents.includes("shutterbugphotos")) && (
+                            <h1 className="section-heading">Submission</h1>
+                            )
+                        }
+                        
+                    {events && events.length>0 && events.map((event,index)=>{
+                        if(index > 0)
+                        {
                             
-                        </Col>
-                    )
-                    }else
-                    return null
-                }else
-                return null
-
-            })}         
-                
-            </Row>
-        </Container>
-
-
-
-            <Container fluid>
-            <h1 className="section-heading">Events</h1>
-            <Row>
-                {events && events.length>0 && events.map((event,index)=>{
-                    if(registeredEvents.includes(event.id))
-                    {
-                        return(
-                            <Col lg={2} md={2} className="item-container" key={index}>
-                                <div className="item-holder">
-                                    <img src={`./posters/${event.id}.jpeg`} className="item-holder-image img-fluid" />
-                                    <div className="registered_overlay">
-                                        <img src={stamp}  className="stamp"/>
+                        }
+                        if( registeredEvents.includes(event.id))
+                        {
+                            if(event.id === "thewallarticle"|| event.id === "thewallartwork"||event.id === "thewallpoetry"||event.id ==="shutterbugshortvideos"||event.id ==="shutterbugphotos")
+                            {
+                                return(
+                                    <Col lg={2} xs={6} md={2} className="item-container" key={index}>
+                                    <div className="item-holder ">
+                                    <Button className="event_register p-0 " onClick={()=>showForm(event)}> 
+                                        <img src={`./icon/${event.id}.png`} className="item-holder-image img-fluid" />
+                                        </Button>
+                                        <div className="item__nameStyle">{event.name}</div>
                                     </div>
-                                    <div className="item__nameStyle">{event.name}</div>
-                                    <h3 className="item-creator">{event.type}</h3>
-                                    <br></br>
-                                    <br></br>
-                                    <br></br>
-                                </div>
-                                
-                            </Col>
-                        )
-                    }else
-                    {
-                        return(
-                            <Col lg={2} md={2} className="item-container" key={index}>
-                                <div className="item-holder">
-                                    <img src={`./posters/${event.id}.jpeg`} className="item-holder-image img-fluid" />
-                                    <div className="item-name">{event.name}</div>
-                                    <h3 className="item-creator">{event.type}</h3>
                                     
-                                    <Button className="event_register" onClick={()=>handleShow(event)}> <span class="major">Register</span> </Button>
-                                </div>
-                            </Col>
-                        )
-                    }
+                                </Col>
+                            )
+                            }else
+                            return null
+                        }else
+                        return null
+        
+                    })}         
+                        
+                    </Row>
+                </Container>
+        
+        
+        
+                    <Container fluid>
+                    <h1 className="section-heading">Events</h1>
+                    <Row>
+                        {events && events.length>0 && events.map((event,index)=>{
+                            if(registeredEvents.includes(event.id))
+                            {
+                                return(
+                                    <Col lg={2} md={2} className="item-container" key={index}>
+                                        <div className="item-holder">
+                                            <img src={`./posters/${event.id}.jpeg`} className="item-holder-image img-fluid" />
+                                            <div className="registered_overlay">
+                                                <img src={stamp}  className="stamp"/>
+                                            </div>
+                                            <div className="item__nameStyle">{event.name}</div>
+                                            <h3 className="item-creator">{event.type}</h3>
+                                            <br></br>
+                                            <br></br>
+                                            <br></br>
+                                        </div>
+                                        
+                                    </Col>
+                                )
+                            }else
+                            {
+                                return(
+                                    <Col lg={2} md={2} className="item-container" key={index}>
+                                        <div className="item-holder">
+                                            <img src={`./posters/${event.id}.jpeg`} className="item-holder-image img-fluid" />
+                                            <div className="item-name">{event.name}</div>
+                                            <h3 className="item-creator">{event.type}</h3>
+                                            
+                                            <Button className="event_register" onClick={()=>handleShow(event)}> <span class="major">Register</span> </Button>
+                                        </div>
+                                    </Col>
+                                )
+                            }
+                        }
+                        )}          
+                        
+                    </Row>
+                </Container>
+                </>
+                    )}
+                    </Col>
+                    </Row>
+          
+                    <EventRegistrationModal show={show} rules={selectedRules} description={selectedDescription}  eventid={selectedId} githubRequired={githubReq} teamNameRequired={teamNameReq} maxTeamMembers={maxTeamMembers} onHide={handleClose}/>
+          
+                    <SubmissionForm  show={showSubmission} onHide={handleClose} eventData={eventData}/>
+                  </Container>
+                          </>
+
                 }
-                )}          
-                
-            </Row>
-        </Container>
-
-            </Col>
-          </Row>
-
-          <EventRegistrationModal show={show} rules={selectedRules} description={selectedDescription}  eventid={selectedId} githubRequired={githubReq} teamNameRequired={teamNameReq} maxTeamMembers={maxTeamMembers} onHide={handleClose}/>
-
-          <SubmissionForm  show={showSubmission} onHide={handleClose} eventData={eventData}/>
-        </Container>
+        }
+        </>
+        
     )
 }
 
